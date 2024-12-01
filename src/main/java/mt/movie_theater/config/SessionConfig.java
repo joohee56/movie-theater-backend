1cd98a2da61e915dde3b58cf8c46e6b8c800d321
Joohui <joohuii96@gmail.com>
2024-12-01 17:56:13 +0900

    [BE] fix: 세션에 도메인 설정

---
+++ src/main/java/mt/movie_theater/config/SessionConfig.java
@@ -0,0 +1,22 @@
+package mt.movie_theater.config;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.SessionCookieConfig;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SessionConfig {
+    @Bean
+    public ServletContextInitializer servletContextInitializer() {
+        return new ServletContextInitializer() {
+            @Override
+            public void onStartup(ServletContext servletContext) {
+                SessionCookieConfig sessionCookieConfig = servletContext.getSessionCookieConfig();
+                sessionCookieConfig.setDomain("http://di2iu03lzjtpc.cloudfront.net"); // 설정할 도메인
+                sessionCookieConfig.setPath("/"); // 설정할 경로
+            }
+        };
+    }
+}
