7e09624d883151594114ef2a712aff53a25dee60
Joohui <joohuii96@gmail.com>
2024-12-04 12:18:19 +0900

    [BE] chore: 쿠키 도메인 . 제거

--- src/main/java/mt/movie_theater/HomeController.java
+++ src/main/java/mt/movie_theater/HomeController.java
@@ -10,6 +10,6 @@ public class HomeController {
 
     @GetMapping("/home")
     public String home() {
-        return "Welcome to Movie Theater5";
+        return "Welcome to Movie Theater6";
     }
 }
