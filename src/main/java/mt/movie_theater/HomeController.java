4201dd7c9cc83859d2cabc112cd8ab6d7837b2c3
Joohui <joohuii96@gmail.com>
2024-12-06 03:06:58 +0900

    [BE] chore: 탄력적 IP 할당 후 ec2 재부팅

--- src/main/java/mt/movie_theater/HomeController.java
+++ src/main/java/mt/movie_theater/HomeController.java
@@ -10,6 +10,6 @@ public class HomeController {
 
     @GetMapping("/home")
     public String home() {
-        return "Welcome to Movie Theater7";
+        return "Welcome to Movie Theater8";
     }
 }
