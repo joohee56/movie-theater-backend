d2bfa97e43c02c9b1b34f779f94ce93202eb83ae
Joohui <joohuii96@gmail.com>
2024-12-06 02:31:23 +0900

    [BE] chore: test 코드 db 설정을 위해 테스트 코드 제거

--- src/main/java/mt/movie_theater/api/exception/UnauthorizedException.java
+++
@@ -1,7 +0,0 @@
-package mt.movie_theater.api.exception;
-
-public class UnauthorizedException extends RuntimeException{
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
